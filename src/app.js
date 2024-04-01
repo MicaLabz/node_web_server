cef723b1813f41448889cbc44624b930abf730ad
Micael <micaelcapo1316@gmail.com>
2024-04-01 15:54:29 -0300

    Modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
-const {envs} = require('./config/env') 
-const {startServer} = require('./server/server')
+//COMO ERA ANTES
+
+// const {envs} = require('./config/env') 
+// const {startServer} = require('./server/server')
+
+//Moderna ECMAS6
+
+import {envs} from './config/env.js'
+import {startServer} from './server/server.js'
 
 const main = () => {
     startServer({
--- src/config/env.js
+++ src/config/env.js
@@ -1,16 +1,13 @@
 //Esto hace que las variables de entorno se setean en objeto process
-require('dotenv').config();
-const {get} = require('env-var'); 
+import env from 'dotenv'
+import envvar from 'env-var'
+
+env.config();
 
 //Configurar las propiedades de las variables, crear objeto
 //PORT: consigue el port del .env, pone que es requerido que es que si no esta no va a funcionar y asPortNumber para que si o si sea para eso
 //PUBLIC_PATH: lo consigue de .env, default es que si no especifica se pone eso y es un string
-const envs = {
-    PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
-}
-
-//y lo exporta con module
-module.exports = {
-    envs
+export const envs = {
+    PORT: envvar.get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: envvar.get('PUBLIC_PATH').default('public').asString()
 }
